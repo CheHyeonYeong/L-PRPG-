154b7fd341861364e91dbbb83d2f3a2dc5fbc162
CheHyeonyeong <gusdudco6@naver.com>
2024-03-24 14:10:44 +0900

    player 객체에 max_hp 상수 추가

---
+++ LoopRPG/src/event/Event.java
@@ -0,0 +1,21 @@
+package event;
+
+import charactor.Player;
+
+import java.util.Random;
+
+public class Event {
+
+    //전투나 회복을 랜덤하게 발생시키는 변수
+    Player player;
+    private Random randomEvent;
+    public Event(){
+
+    }
+    public void box(){
+
+    }
+    void recover(){
+        // HP_MAX 값으로 바꾼 후 반환
+    }
+}
